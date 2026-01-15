99f978e3b686c1a57e550c8790103147d9198851
Kris Wong <krischwong@gmail.com>
2026-01-14 23:13:32 -0500

    basic styling for messenger area

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <ul id="messages"></ul>
+      <form
+        id="form"
+        className="fixed right-0 bottom-0 left-0 box-border flex h-12 bg-orange-100 p-1"
+        action=""
+      >
+        <input
+          id="input"
+          className="m-1 grow rounded-4xl border-none bg-amber-400 px-8 py-0 outline-none"
+          autoComplete="off"
+        />
+        <button className="m-1 rounded border-none bg-yellow-400 px-4 py-0 text-gray-900 outline-none">
+          Send
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
