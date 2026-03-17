47a23de60f979828ffce95e2c7acb797358b4ace
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-03-16 22:51:10 -0400

    Navbar and footer

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
     <div>
+      <Navigation />
       <Outlet />
+      <Footer />
     </div>
   );
 };
