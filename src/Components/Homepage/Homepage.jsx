1155b336052dd5eef3687dd452444fec913f73ec
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-04-14 23:11:35 -0400

    removed links

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
     <>
-      <Link to="/profile">Profile</Link>
+      {/* <Link to="/profile">Profile</Link>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
-      <Link to="/chat">Chat</Link>
+      <Link to="/chat">Chat</Link> */}
     </>
   );
 };
