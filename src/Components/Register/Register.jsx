00a021bd061fdd4e1c315e90c75d49a16014f53d
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-04-16 22:03:58 -0400

    Added validation for registration form

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -9,31 +9,58 @@ const Register = () => {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
+    console.log(data, "123");
   };
 
-  console.log(watch("username"));
+  console.log(watch(errors.email));
 
   return (
     <>
-      <div>
-        <form onSubmit={handleSubmit(onsubmit)}>
-          <input
-            placeholder="Email"
-            {...register("email", { required: true })}
-          />
-          <input
-            placeholder="Username"
-            {...register("username", { required: true })}
-          />
-          <input
-            placeholder="Password"
-            {...register("password", { required: true })}
-          />
-          <input
-            placeholder="Password"
-            {...register("password", { required: true })}
-          />
+      <div className="w-1/3">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+          <div className="flex flex-col">
+            <label>Email</label>
+            <input
+              className="my-1 rounded border border-white p-1"
+              placeholder="Email"
+              {...register("email", { required: true })}
+            />
+            {errors.email && <p className="text-red-500">Email is required</p>}
+          </div>
+          <div className="my-4 flex flex-col">
+            <label>Username</label>
+            <input
+              className="my-1 rounded border border-white p-1"
+              placeholder="Username"
+              {...register("username", { required: true })}
+            />
+            {errors.username && (
+              <p className="text-red-500">Username is required</p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label>Password</label>
+            <input
+              className="my-1 rounded border border-white p-1"
+              placeholder="Password"
+              {...register("password", { required: true })}
+            />
+            <label>Confirm Password</label>
+            <input
+              className="my-1 rounded border border-white p-1"
+              placeholder="Password"
+              {...register("confirmPassword", {
+                required: "Please confirm your password",
+                validate: (value) => {
+                  value === watch("password") || "Passwords must match!";
+                },
+              })}
+            />
+            {errors.confirmPassword && (
+              <p className="text-red-500">{errors.confirmPassword.message}</p>
+            )}
+          </div>
+          <input type="submit" value="Create Account" />
         </form>
       </div>
     </>
