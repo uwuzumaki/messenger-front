3227bd011863488fdec4387a2fb55b8e92de7318
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-13 01:19:02 -0400

    Added context to layout

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,15 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { data, Outlet } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import Footer from "../Footer/Footer";
+import { AuthProvider, AuthContext } from "../../contexts/authContext";
+import { use, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div className="flex flex-1 flex-col">
-      <Navigation />
-      <div className="flex flex-1 flex-col items-center justify-center bg-slate-300">
-        <Outlet />
-      </div>
-      <Footer />
+      <AuthProvider>
+        <Navigation />
+        <div className="flex flex-1 flex-col items-center justify-center bg-slate-300">
+          <Outlet />
+        </div>
+        <Footer />
+      </AuthProvider>
     </div>
   );
 };
