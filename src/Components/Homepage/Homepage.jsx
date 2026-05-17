861135beb0fa71c7e2eccf6f0c9cfdba4d729e70
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-16 20:29:21 -0400

    Conditional rendering of homepage items

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -7,21 +7,38 @@ const Homepage = () => {
   return (
     <div className="flex w-1/4 flex-1 flex-col items-center justify-center">
       <div className="mb-8 text-3xl">Welcome!</div>
-      <div className="flex w-full justify-around">
-        <Link
-          className="mx-2 rounded-lg border border-solid border-indigo-500 bg-indigo-500 px-2 py-1 text-white"
-          to="/profile"
-        >
-          Profile
-        </Link>
-        <Link
-          className="mx-2 rounded-lg border border-solid border-yellow-500 bg-yellow-500 px-2 py-1 text-black"
-          to="/chat"
-        >
-          Chat
-        </Link>
-        <>{auth.user}</>
-      </div>
+      {auth.authenticated ? (
+        <div className="flex w-full justify-around">
+          <Link
+            className="mx-2 rounded-lg border border-solid border-indigo-500 bg-indigo-500 px-2 py-1 text-white"
+            to="/profile"
+          >
+            Profile
+          </Link>
+          <Link
+            className="mx-2 rounded-lg border border-solid border-yellow-500 bg-yellow-500 px-2 py-1 text-black"
+            to="/chat"
+          >
+            Chat
+          </Link>
+          <>{auth.user}</>
+        </div>
+      ) : (
+        <div className="flex w-full justify-around">
+          <Link
+            className="mx-2 rounded-lg border border-solid border-indigo-500 bg-indigo-500 px-2 py-1 text-black"
+            to="/login"
+          >
+            Login
+          </Link>
+          <Link
+            className="mx-2 rounded-lg border border-solid border-yellow-500 bg-yellow-500 px-2 py-1 text-black"
+            to="/register"
+          >
+            Registration
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
