9413d04771def5190fbdc966ada0c5da8604dd52
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-18 14:36:53 -0400

    Wrapped app with loading page provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import routes from "./routes.jsx";
+import { LoadingProvider } from "./contexts/loadingContext.jsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <LoadingProvider>
+      <RouterProvider router={router} />
+    </LoadingProvider>
   </StrictMode>,
 );
