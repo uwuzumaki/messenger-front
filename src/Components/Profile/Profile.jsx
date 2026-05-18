ad744a7060c8bf90b6c74f8cae9cb205213dbcc7
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-18 16:37:46 -0400

    removed rendering logic from page

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,14 +1,4 @@
-import { useEffect } from "react";
-import { useLoaderData, useNavigate } from "react-router-dom";
-
 const Profile = () => {
-  const loaderData = useLoaderData();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    !loaderData.authenticated ? navigate("/unauthorized") : null;
-  });
-
   return (
     <>
       <div className="mx-auto flex h-full flex-1 flex-col justify-evenly">
