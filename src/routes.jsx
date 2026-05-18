79720851dbf76906a93076ce37b04a1e880618a7
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-18 17:40:58 -0400

    Added redirect loader for when user is logged in

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,23 @@
 import Layout from "./Components/Layout/Layout";
 import Homepage from "./Components/Homepage/Homepage";
+import LandingPage from "./Components/LandingPage/LandingPage";
 import Profile from "./Components/Profile/Profile";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import Chat from "./Components/Chat/Chat";
-import authLoader from "./loaders/authLoader";
 import LoggedOut from "./Components/LoggedOut/LoggedOut";
+import authLoader from "./loaders/authLoader";
+import redirectLoader from "./loaders/redirectLoader";
+
 const routes = [
   {
     element: <Layout />,
     loader: authLoader,
     id: "root",
     children: [
+      { path: "/home", element: <LandingPage /> },
       {
         path: "/profile",
-        loader: authLoader,
         element: <Profile />,
       },
       {
@@ -26,6 +29,7 @@ const routes = [
   {
     path: "/",
     element: <Layout />,
+    loader: redirectLoader,
     children: [
       {
         index: true,
