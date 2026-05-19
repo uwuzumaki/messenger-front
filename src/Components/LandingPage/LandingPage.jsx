11b29a1e28256ec0895b5cc3f4a895a5c018e9e9
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-19 15:09:15 -0400

    Updated landing page

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
   return (
-    <div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+    <div className="flex w-1/4 flex-1 flex-col items-center justify-center">
+      <div className="mb-8 text-3xl">Welcome!</div>
+      <div className="flex w-full justify-around">
+        <Link
+          className="mx-2 rounded-lg border border-solid border-indigo-500 bg-indigo-500 px-2 py-1 text-black"
+          to="/Chat"
+        >
+          Chat
+        </Link>
+        <Link
+          className="mx-2 rounded-lg border border-solid border-yellow-500 bg-yellow-500 px-2 py-1 text-black"
+          to="/profile"
+        >
+          Profile
+        </Link>
+      </div>
     </div>
   );
 };
