fa3a3dec80f194f38a40e8444f01d1a9ab64bf41
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-25 15:52:59 -0400

    Centering landing page

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div className="flex w-1/4 flex-1 flex-col items-center justify-center">
+    <div className="m-auto flex w-1/4 flex-1 flex-col items-center justify-center">
       <div className="mb-8 text-3xl">Welcome!</div>
       <div className="flex w-full justify-around">
         <Link
