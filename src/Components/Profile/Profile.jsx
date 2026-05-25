c4cce301da8fbc64978f2832fe3113a8889286df
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-25 16:03:11 -0400

    some profile styling

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,9 +1,20 @@
+import { use, useEffect } from "react";
+import { AuthContext } from "../../contexts/authContext.jsx";
+
 const Profile = () => {
+  const auth = use(AuthContext);
+
   return (
     <>
-      <div className="mx-auto flex h-full flex-1 flex-col justify-evenly">
-        <div>Email</div>
-        <div>Username</div>
+      <div className="ml-28 flex h-full flex-1 flex-col justify-evenly">
+        <div>
+          <p class="text-heading text-4xl font-black text-emerald-400 text-shadow-emerald-900 text-shadow-md">
+            Username
+          </p>
+          <div className="text-heading text-2xl font-bold text-blue-400 text-shadow-blue-900 text-shadow-md">
+            {auth.user}
+          </div>
+        </div>
         <div>Cheese</div>
       </div>
     </>
